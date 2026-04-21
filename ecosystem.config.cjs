e79a4e85b3389c4004dd89b3546032a4442bd7b2
Mostafa Kamel <mkamel30@gmail.com>
2026-04-21 16:06:38 +0200

    fix: rename pm2 config to .cjs and fix server path

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'onboarding-server',
-      script: 'dist/server/index.js',
+      script: 'dist/server/server/index.js',
       instances: 1,
       autorestart: true,
       watch: false,
